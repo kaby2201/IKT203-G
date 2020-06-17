76e4cac193bdfb830c0c19e9eb04d7294d1db2ff
Byamungu Kabiraba <byamuk16@student.uia.no>
2020-06-18 00:55:20 +0200

    added some comments

--- java/non_primitive_data_structures/src/linear/_static/Main.java
+++ java/non_primitive_data_structures/src/linear/_static/Main.java
@@ -1,7 +1,28 @@
+/*
+    Java- Autoboxing:
+
+    byte, short, int, long, double, char boolean are all primitive types.
+    The memory size will be set: Example:
+        - byte  => size of 1byte
+        - short => 2 bytes
+        - int  => 4 bytes
+        - char => 2 bytes
+        ...
+    Primitive types are not object, so they don't have toString, hasCode, toEquals ect.
+    In java your have a wrapper for many of this types, they are all capitalize.
+    Remember we don't have wrapper for int is Integer.
+
+    Java will try to switch between those types by something called autoboxing.
+    See Java doc for more.
+ */
 package linear._static;
 
 public class Main {
     public static void main(String[] args) {
+        char[] alpha = new char[26];
 
+        for (int i = 0; i < alpha.length; i++) {
+            alpha[i] = (char) ('A'+i);
+        }
     }
 }
